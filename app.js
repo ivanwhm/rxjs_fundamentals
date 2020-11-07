d663ecada106f42ebba9c73e6aba9a05092a2fad
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-07 13:17:20 -0300

    Unicast Observables

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const { Observable } = Rx;
+const { tap } = RxOperators;
 
 const observable = new Observable((subscriber) => {
-  // Throw the value 1 into the pipeline
+  // Throw the values into the pipeline
   subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
 
   // Marks the observable as complete, no more values will come out
   subscriber.complete();
 
   // Emit an error, no more values will come out
   subscriber.error(new Error("Something bad happen"));
-});
+}).pipe(tap((value) => console.info(value)));
 
 observable.subscribe(
   (value) => console.log("Next value:", value), // next
@@ -17,4 +20,10 @@ observable.subscribe(
   () => console.log("Complete") //completion
 );
 
-observable;
+observable.subscribe(
+  (value) => console.log("From 2nd subscribe", value), // next
+  (err) => console.error("Bad Thing", err), //error
+  () => console.log("Complete") //completion
+);
+
+new Observable(() => {});
