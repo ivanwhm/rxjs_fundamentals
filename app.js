f7b21e5409efc8d3b18d60b461912e28f46ed23c
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-07 12:56:19 -0300

    Specific Operators

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const { fromEvent } = Rx;
-const { map } = RxOperators;
+const { map, pluck } = RxOperators;
 
 const input = document.createElement("input");
 const container = document.querySelector(".container");
 container.appendChild(input);
 
-const observable = fromEvent(input, "input").pipe(
-  map((event) => event.target.value),
-  map((value) => parseInt(value)),
-  map((value) => {
-    if (isNaN(value)) {
-      throw new Error("Enter a number.");
-    }
-    return value;
-  })
-);
+const observable = fromEvent(input, "input") //
+  .pipe(
+    pluck("target", "value"),
+    map((value) => parseInt(value)),
+    map((value) => {
+      if (isNaN(value)) {
+        throw new Error("Enter a number.");
+      }
+      return value;
+    })
+  );
 
 observable.subscribe({
   next(value) {
