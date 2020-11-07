e3e1e3f6a25771a33b9ad871363117cf79548590
Ivan Wilhelm <ivan.whm@icloud.com>
2020-11-07 13:09:07 -0300

    Low Level Observables

--- app.js
+++ app.js
@@ -1,46 +1,26 @@
-const { fromEvent } = Rx;
-const { map, pluck } = RxOperators;
+const { Observable } = Rx;
 
-const input = document.createElement("input");
-const container = document.querySelector(".container");
-container.appendChild(input);
+const observable = new Observable((subscriber) => {
+  // Throw the value 1 into the pipeline
+  subscriber.next(1);
 
-const observable = fromEvent(input, "input") //
-  .pipe(
-    pluck("target", "value"),
-    map((value) => parseInt(value)),
-    map((value) => {
-      if (isNaN(value)) {
-        throw new Error("Enter a number.");
-      }
-      return value;
-    })
-  );
+  // Marks the observable as complete, no more values will come out
+  subscriber.complete();
+
+  // Emit an error, no more values will come out
+  subscriber.error(new Error("Something bad happen"));
+});
 
 observable.subscribe({
   next(value) {
-    console.log(`Your value is: ${value}`);
+    console.log(`Got a value: ${value}`);
+  },
+  complete() {
+    console.log("Observable is complete. Don't expect any more values.");
   },
   error(err) {
-    console.error("Bad Thing Happen!!", err.message);
+    console.error("Bad Thing!", err);
   },
 });
 
 observable;
-
-// const input = document.querySelector("input");
-
-// input.addEventListener("input", (event) => {
-//   let text = event.target.value;
-//   text = parseInt(text);
-
-//   if (isNaN(text)) {
-//     throw new Error("You must enter a number.");
-//   }
-
-//   logValue(text);
-// });
-
-// const logValue = (value) => {
-//   console.log(`Your value: ${value}`);
-// };
